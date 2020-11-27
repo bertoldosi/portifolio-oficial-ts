e0b46837c5f5e4c6a072720e5ceeac7c6b606b00
bertoldosi <matheus.bertoldosi@gmail.com>
2020-11-27 12:47:54 -0500

    :art: Ajustando rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 import Home from '../pages/Home/home'
 import Login from '../pages/Login/login';
@@ -8,11 +8,11 @@ import Projeto from '../pages/Projeto/projeto'
 
 const Routes = function () {
     return (
-        <Switch>
+        <HashRouter>
             <Route exact path='/' component={Home} />
             <Route path='/projetos' component={Projeto} />
             <Route path='/login' component={Login} />
-        </Switch>
+        </HashRouter>
     )
 }
 
