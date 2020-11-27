74824a033ce94eb0ac6bb55c2c1ef14a90ded75d
bertoldosi <matheus.bertoldosi@gmail.com>
2020-11-27 12:15:20 -0500

    Updates

---
+++ precache-manifest.7c6fb1c54c9f8cd28859eb224224f967.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5376eaf2879546aec8de8af8ff6283c3",
+    "url": "/bertoldosi/portifolio-oficial-ts/index.html"
+  },
+  {
+    "revision": "4ab8942b263563ece61d",
+    "url": "/bertoldosi/portifolio-oficial-ts/static/js/2.fc292b23.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/bertoldosi/portifolio-oficial-ts/static/js/2.fc292b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7632bccef7f1c0f0346f",
+    "url": "/bertoldosi/portifolio-oficial-ts/static/js/main.3511e6b6.chunk.js"
+  },
+  {
+    "revision": "2540cc1882194780554f",
+    "url": "/bertoldosi/portifolio-oficial-ts/static/js/runtime-main.3c5e5f38.js"
+  },
+  {
+    "revision": "060f60f6affdd03109f053f6d3355fc9",
+    "url": "/bertoldosi/portifolio-oficial-ts/static/media/logo.060f60f6.png"
+  },
+  {
+    "revision": "7ee69812419d7bb5bc42bd6b4c38dc2a",
+    "url": "/bertoldosi/portifolio-oficial-ts/static/media/profile.7ee69812.jpeg"
+  }
+]);
